00d3dbea8fc7cc201b47b486fe9e8cdd6ec5f371
hyukjunkim1116 <nmkk1234@naver.com>
2024-02-22 22:33:53 +0900

    complete v2

--- config/settings.py
+++ config/settings.py
@@ -44,8 +44,8 @@ AWS_DEFAULT_ACL = None
 
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10000),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=14400),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=1000),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
